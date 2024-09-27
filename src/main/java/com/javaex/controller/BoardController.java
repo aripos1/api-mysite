fb2a1923ebc1d4745d3a3883dbbb19db4e8da12b
aripos1 <ihsjjang@gmail.com>
2024-09-27 18:12:59 +0900

    20240927

--- src/main/java/com/javaex/controller/BoardController.java
+++ src/main/java/com/javaex/controller/BoardController.java
@@ -3,57 +3,51 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.BoardService;
+import com.javaex.util.JsonResult;
 import com.javaex.vo.BoardVo;
 import com.javaex.vo.UserVo;
 
 import jakarta.servlet.http.HttpSession;
 
-@Controller
+@RestController
 public class BoardController {
 
 	@Autowired
 	private BoardService boardService;
-	// 필드
 
-	// 생성자
-
-	// 메서드 gs
-
-	// 메서드 일반
-
-	// 메인
-	@RequestMapping(value = "/board/list", method = { RequestMethod.GET, RequestMethod.POST })
-	public String boardList(Model model) {
+	@GetMapping("api/boardlists")
+	public JsonResult boardList() {
 
 		System.out.println("boardList");
-
 		List<BoardVo> boardList = boardService.exeBoardList();
-		model.addAttribute("boardList", boardList);
-		boardService.exeBoardList();
-
-		return "board/list";
+		
+		if(boardList != null) {
+			return JsonResult.success("성공"); 
+			
+		}else {
+			return JsonResult.fail("리스트 불러오기 실패"); 
+		}
+		
+	
 	}
 
 	@RequestMapping(value = "/board/read", method = { RequestMethod.GET, RequestMethod.POST })
-	public String boardRead(@ModelAttribute BoardVo boardVo,HttpSession session,Model model) {
+	public String boardRead(@ModelAttribute BoardVo boardVo, HttpSession session, Model model) {
 		System.out.println("UserController.login()");
-		
-		
+
 		BoardVo boardOne = boardService.exeBoardOne(boardVo);
 		model.addAttribute("BoardVo", boardOne);
-		
-		
+
 		System.out.println(boardOne);
-	
 
 		return "board/read";
 	}
@@ -89,16 +83,16 @@ public class BoardController {
 	}
 
 	@RequestMapping(value = "/board/modifyform", method = { RequestMethod.GET, RequestMethod.POST })
-	public String modifyform(@ModelAttribute BoardVo boardVo,HttpSession session,Model model) {
+	public String modifyform(@ModelAttribute BoardVo boardVo, HttpSession session, Model model) {
 		System.out.println("modifyform");
-		
+
 		BoardVo boardOne = boardService.exeBoardOne(boardVo);
 		model.addAttribute("BoardVo", boardOne);
-				
+
 		System.out.println(boardOne);
 		return "/board/modifyForm";
 	}
-	
+
 	@RequestMapping(value = "board/modify", method = { RequestMethod.GET, RequestMethod.POST })
 	public String modify(HttpSession session, @ModelAttribute BoardVo boardVo) {
 		System.out.println("modify");
--- src/main/java/com/javaex/controller/GuestbookController.java
+++ src/main/java/com/javaex/controller/GuestbookController.java
@@ -3,14 +3,11 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.GuestbookService;
@@ -40,5 +37,34 @@ public class GuestbookController {
 
 		return "redirect:/";
 	}
+	@GetMapping("/api/guestbooks/{no}")
+	public GuestVo getGuestOne(@PathVariable(value = "no") int no) {
+
+		GuestVo guestVo = guestbookService.exeDeleteForm(no);
+
+		System.out.println(guestVo);
+		return guestVo;
+	}
+
+	@DeleteMapping("/api/guestbooks/{no}")
+	public boolean delete(@PathVariable(value = "no") int no, @RequestBody GuestVo guestVo) {
+		String password = guestVo.getPassword(); // 요청에서 비밀번호 가져오기
+		boolean deleted = guestbookService.exeDelete(no, password);
+		return deleted;
+	}
+	
+//	/* 회원정보가져오기 */
+//	@GetMapping("/api/users/me")
+//	public JsonResult modifyForm(HttpServletRequest request) {
+//		int no = JwtUtil.getNoFromHeader(request);
+//		if (no != -1) {
+//			// 정상
+//			UserVo userVo = userService.exeModifyForm(no);
+//			return JsonResult.success(userVo);
+//		} else {
+//			// 토큰이 없거나(로그인상태아님), 변조된 경우
+//			return JsonResult.fail("토큰X, 비로그인, 변조");
+//		}
+//	}
 	
 }
